9af93b3f4897e06adf52c0e0ce2c8fc21c0c6274
Sanjay Saravanan <sanjay.saravanan@encora.com>
2021-11-06 16:29:49 +0530

    added few more changes

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,5 +1,34 @@
-import React from 'react';
+/* eslint-disable */
+import React from "react";
+const Login = () => {
+  const searchStore = () => {
+    alert("API Started");
+    // setState({ ..._state, searchLoading: true });
+    fetch("http://localhost:2001/admin-bff/info/salesrep/store", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        apiToken: "89c7fc4a1d056460cac80376ad114820",
+        searchKey: "15380",
+      }),
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        const {
+          result: [data],
+        } = json;
+        console.log(data);
+      });
+  };
 
-const Login = () => <div>Login</div>;
+  return (
+    <div>
+      Login
+      <button type="button" onClick={searchStore}>
+        Click
+      </button>
+    </div>
+  );
+};
 
 export default Login;
--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -4,7 +4,13 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import {
-  Grid, Form, Segment, Button, Header, Message, Icon,
+  Grid,
+  Form,
+  Segment,
+  Button,
+  Header,
+  Message,
+  Icon,
 } from 'semantic-ui-react';
 
 const Register = () => {
@@ -20,12 +26,45 @@ const Register = () => {
         </Header>
         <Form size="large">
           <Segment stacked>
-            <Form.Input fluid name="username" icon="user" iconPosition="left" placeholder="Username" onChange={handleChange} type="text" />
-            <Form.Input fluid name="email" icon="mail" iconPosition="left" placeholder="Email Address" onChange={handleChange} type="email" />
-            <Form.Input fluid name="password" icon="lock" iconPosition="left" placeholder="Password" onChange={handleChange} type="password" />
-            <Form.Input fluid name="passwordConfirmation" icon="repeat" iconPosition="left" placeholder="Password Confirmation " onChange={handleChange} type="password" />
-            <Button color="orange" fluid size="large">Submit</Button>
-
+            <Form.Input
+              fluid
+              name="username"
+              icon="user"
+              iconPosition="left"
+              placeholder="Username"
+              onChange={handleChange}
+              type="text"
+            />
+            <Form.Input
+              fluid
+              name="email"
+              icon="mail"
+              iconPosition="left"
+              placeholder="Email Address"
+              onChange={handleChange}
+              type="email"
+            />
+            <Form.Input
+              fluid
+              name="password"
+              icon="lock"
+              iconPosition="left"
+              placeholder="Password"
+              onChange={handleChange}
+              type="password"
+            />
+            <Form.Input
+              fluid
+              name="passwordConfirmation"
+              icon="repeat"
+              iconPosition="left"
+              placeholder="Password Confirmation "
+              onChange={handleChange}
+              type="password"
+            />
+            <Button color="orange" fluid size="large">
+              Submit
+            </Button>
           </Segment>
         </Form>
         <Message>
